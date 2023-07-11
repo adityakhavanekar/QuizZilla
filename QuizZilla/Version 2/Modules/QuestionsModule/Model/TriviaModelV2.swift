232b573b125f87fc451597b9b56533ab5de3d492
AdityaNeoK <adityakhavanekar98@gmail.com>
2023-07-11 12:54:08 +0530

    Added Model V2

---
+++ QuizZilla/Version 2/Modules/QuestionsModule/Model/TriviaModelV2.swift
@@ -0,0 +1,34 @@
+//
+//  TriviaModelV2.swift
+//  QuizZilla
+//
+//  Created by APPLE on 11/07/23.
+//
+
+import Foundation
+
+struct TriviaV2: Codable {
+    let id, question: String
+    let options: [String]
+    let ca: String
+
+    enum CodingKeys: String, CodingKey {
+        case id = "_id"
+        case question, options
+        case ca = "CA"
+    }
+}
+
+struct TriviaElementV2: Codable {
+    let id, question: String
+    let options: [String]
+    let ca: String
+    let imageURL: String?
+
+    enum CodingKeys: String, CodingKey {
+        case id = "_id"
+        case question, options
+        case ca = "CA"
+        case imageURL = "imageUrl"
+    }
+}
