fe9afb68d8778a090dfac0268ca9d23fed2fafe2
Aditya Khavanekar <adityakhavanekar98@gmail.com>
2023-07-23 19:17:37 +0530

    Added Model and ViewModel for home

---
+++ QuizZilla/Version 2/Modules/HomeModule/Model/HomeModelV2.swift
@@ -0,0 +1,8 @@
+//
+//  HomeModelV2.swift
+//  QuizZilla
+//
+//  Created by APPLE on 23/07/23.
+//
+
+import Foundation
---
+++ QuizZilla/Version 2/Modules/HomeModule/ViewModel/HomeViewModelV2.swift
@@ -0,0 +1,8 @@
+//
+//  HomeViewModelV2.swift
+//  QuizZilla
+//
+//  Created by APPLE on 23/07/23.
+//
+
+import Foundation
